d9bb17cbf195c40ed3ea7842f7bcf09eb4578e17
JWSch4fer <joeschafer28@gmail.com>
2025-03-25 09:09:20 -0400

    start abstract syntax tree

---
+++ parser/parser.go
@@ -0,0 +1,43 @@
+package parser
+
+import (
+	"github.com/JWSch4fer/interpreter/ast"
+	"github.com/JWSch4fer/interpreter/lexer"
+	"github.com/JWSch4fer/interpreter/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	currToken token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	//read so curr and peek are defined
+	p.NextToken()
+	p.NextToken()
+
+	return p
+}
+
+func (p *Parser) NextToken() {
+	p.currToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.currToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.NextToken()
+	}
+	return program
+}
