2696179dc578d1432efac60b70c7901873d31e86
JWSch4fer <joeschafer28@gmail.com>
2025-03-25 07:29:02 -0400

    add repl

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/JWSch4fer/interpreter/repl"
+)
 
 func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s\n", user.Username)
 	fmt.Println("starting interpreter...")
+	repl.Start(os.Stdin, os.Stdout)
 }
