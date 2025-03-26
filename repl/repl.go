c3b210e0315440b03e4a4c3bbb00aecea0e52806
JWSch4fer <joeschafer28@gmail.com>
2025-03-26 16:32:36 -0400

    update repl

--- repl/repl.go
+++ repl/repl.go
@@ -6,10 +6,14 @@ import (
 	"io"
 
 	"github.com/JWSch4fer/interpreter/lexer"
-	"github.com/JWSch4fer/interpreter/token"
+	"github.com/JWSch4fer/interpreter/parser"
 )
 
 const PROMPT = ">>"
+
+const ERRORSEP = "\x1b[38;5;208m" + `
+====================================================================
+` + "\x1b[0m"
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
@@ -22,8 +26,25 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
 	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, ERRORSEP)
+	io.WriteString(out, "Something Has Interrupted Internal Execution; Error Thrown.\n")
+	io.WriteString(out, "Check Syntax...\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
+	io.WriteString(out, ERRORSEP)
 }
