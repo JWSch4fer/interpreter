42e183eff3e3b737cb5984b02c5dfeabe18e0ff1
JWSch4fer <joeschafer28@gmail.com>
2025-03-27 14:54:37 -0400

    add evaluation of infix operators for integers

--- evaluate/evaluate.go
+++ evaluate/evaluate.go
@@ -25,6 +25,10 @@ func Eval(node ast.Node) object.Object {
 	case *ast.PrefixExpression:
 		right := Eval(node.Right)
 		return evalPrefixExpression(node.Operator, right)
+	case *ast.InfixExpression:
+		left := Eval(node.Left)
+		right := Eval(node.Right)
+		return evalInfixExpression(node.Operator, left, right)
 
 	//Expression typees
 	case *ast.IntegerLiteral:
@@ -80,6 +84,7 @@ func evalBangOperatorExpression(right object.Object) object.Object {
 	}
 }
 
+// specify behaviour of minus operator
 func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
 	if right.Type() != object.INTEGER_OBJ {
 		return NULL
@@ -87,4 +92,38 @@ func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
 
 	value := right.(*object.Integer).Value
 	return &object.Integer{Value: -value}
+}
+
+// specify behaviour of minus operator
+func evalInfixExpression(operator string, left, right object.Object) object.Object {
+	switch {
+	case
+		left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
+		return evalIntegerInfixExpression(operator, left, right)
+	default:
+		return NULL
+
+	}
+}
+
+func evalIntegerInfixExpression(
+	operator string,
+	left, right object.Object,
+) object.Object {
+	leftVal := left.(*object.Integer).Value
+	rightVal := right.(*object.Integer).Value
+
+	switch operator {
+	case "+":
+		return &object.Integer{Value: leftVal + rightVal}
+	case "-":
+		return &object.Integer{Value: leftVal - rightVal}
+	case "/":
+		return &object.Integer{Value: leftVal / rightVal}
+	case "*":
+		return &object.Integer{Value: leftVal * rightVal}
+	default:
+		return NULL
+
+	}
 }
